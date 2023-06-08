82f441e3375d3bf6d9d1396619287317ec23a163
HaroldS10 <haroldsanty13@gmail.com>
2023-06-08 15:36:12 -0500

    prints different combinations of two digits.

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i < 89 and i / 10 < i % 10:
-        print("{:02d}".format(i), end=", ")
-    elif i == 89:
-        print("{:2d}".format(i))
+for i in range(0, 9):
+    for j in range(0, 10):
+        if (i != j) and (i < j) and (i != 8):
+            print("{:d}{:d}".format(i, j), end=", ")
+print("{:d}{:d}".format(8, 9))
