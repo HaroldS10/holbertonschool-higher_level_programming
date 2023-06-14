0001d9d22a1744845e79a60b821c3999a41687d4
HaroldS10 <haroldsanty13@gmail.com>
2023-06-14 15:01:57 -0500

    defines a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""empty class Square that defines a square"""
+""" empty class Square that defines a square """
 
 
 class Square:
-    """Empty class - Pass is used"""
+    """ Empty class - Pass is used """
     pass 
