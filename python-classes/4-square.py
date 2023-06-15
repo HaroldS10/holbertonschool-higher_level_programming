b5565c8fea079e228e42a1fe3256f85ddbf89596
HaroldS10 <haroldsanty13@gmail.com>
2023-06-15 11:06:40 -0500

    property setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,12 +7,10 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
 
-
     @property
     def size(self):
         """Getter for size"""
         return self.__size
-
 
     @size.setter
     def size(self, value):
