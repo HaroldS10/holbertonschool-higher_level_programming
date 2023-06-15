f3fb8399ab519259cf026c376c3e82511bbcc7e9
HaroldS10 <haroldsanty13@gmail.com>
2023-06-15 10:32:33 -0500

    Returns the current square area

---
+++ python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Square - Holbertoon's Python class """
+
+
+class Square:
+    """Init method to instantiation"""
+    def __init__(self, size=0):
+        if (isinstance(size, int) is False):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area (self):
+        """Returns the current square area"""
+        return self.__size ** 2
