47229c2baa148629845b8337128d1c0dd1bbca85
HaroldS10 <haroldsanty13@gmail.com>
2023-06-16 13:34:59 -0500

    adds 2 integers

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""adds 2 integers"""
+
+
+def add_integer(a, b=98):
+    """Adds two integers.
+        Arguments:
+        Datatypes
+    """
+    if type(a) is not [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) is not [int, float]:
+        raise TypeError("b must be an integer")
+    """
+    Return: sum of the two integers
+    """
+    return (int(a) + int(b))
