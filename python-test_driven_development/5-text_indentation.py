66fdf47dbc4a8139a99b57140ce040fa362f1673
HaroldS10 <haroldsanty13@gmail.com>
2023-06-21 12:17:53 -0500

    finished projects

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Definning a text identation function.
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines.
+    Arguments:
+        dataype:
+    Raises:
+        TypeError (text): Text must be a string.
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text) and text[i] == ' ':
+        i += 1
+
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] == "\n" or text[i] in ".?:":
+            if text[i] in ".?:":
+                print("\n")
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        i += 1
